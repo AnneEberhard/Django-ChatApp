8afda00f475da72775d8769a80ed8492d27af7ee
AnneEberhard <anne.eberhard@gmx.net>
2024-01-20 09:48:59 +0100

    added form action and def index

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
 def index(request):
+    if request.method == 'POST':
+        print("Reiceived data " + request.POST['textmessage'])
     return render(request, 'chat/index.html', {'username': 'Anne'})
