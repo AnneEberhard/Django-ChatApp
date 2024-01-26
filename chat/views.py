8a5ce3040b9ee0710e49c7dcee950e080c0afb09
AnneEberhard <anne.eberhard@gmx.net>
2024-01-26 17:16:45 +0100

    added message js

--- chat/views.py
+++ chat/views.py
@@ -10,13 +10,14 @@ from django.contrib.auth.hashers import make_password
 
 @login_required(login_url='/login/')
 def index(request):
-    username = request.user.first_name if request.user.is_authenticated else "DefaultUsername"
-    if request.method == 'POST':
+    username = request.user.first_name if request.user.is_authenticated else "DefaultUsername" 
+    if request.method == 'POST':    
         print("Received data " + request.POST['textmessage'])
         myChat = Chat.objects.get(id=1)
         Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
-    chatMessages = Message.objects.filter(chat__id=1)
-    return render(request, 'chat/index.html', {'messages': chatMessages,'username': username })
+    chatMessages = Message.objects.filter(chat__id=1) 
+    return render(request, 'chat/index.html', {'messages': chatMessages,'username': username }) 
+
 
 
 def loginView(request):
@@ -32,19 +33,6 @@ def loginView(request):
         else:
             return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
     return render(request, 'auth/login.html', {'redirect': redirect})
-
-#def registerView(request):
-#    if request.method == 'POST':
-#        username = request.POST.get('username')
-#        email = request.POST.get('email')
-#        password = request.POST.get('password')
-#        repeatPassword = request.POST.get('repeatPassword')
-#        if password == repeatPassword:
-#            user = User.objects.create_user(username, email, password)
-#            return render(request, 'auth/login.html')
-#        else:
-#            return render(request, 'auth/register.html', {'passwordNoMatch': True})    
-#    return render(request, 'auth/register.html')
 
 
 def registerView(request):
