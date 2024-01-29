56fdc8d30b084d766291eecbffbad041c44c909d
AnneEberhard <anne.eberhard@gmx.net>
2024-01-29 17:29:40 +0100

    added collectstatic

--- django_chat_app/urls.py
+++ django_chat_app/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from chat.views import index, loginView, logoutView, registerView
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +27,4 @@ urlpatterns = [
     path('login/', loginView),
     path('register/', registerView),
     path('logout/', logoutView),
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
